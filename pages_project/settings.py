c1e3299f1cf3bbe7622d4d399f00afdd8771035b
mashabliznina <bliznina.mi@gmail.com>
2021-09-29 20:54:51 +0100

    detect DEBUG mode from .env

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('ISDEBUG') == "True"
 
 ALLOWED_HOSTS = ['websiteforpodcast.herokuapp.com', '127.0.0.1']
 
